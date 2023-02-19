b91c63104509576bba4edf8c825bb4cec9e2b701
olkm-sasaki-ren <olkm-sasaki-ren@gmail.com>
2023-02-19 20:03:40 +0900

    finish 7a

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,39 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+5.times do |n|
+  User.create!(
+    email: "sample#{n + 1}@sample",
+    name: "sample#{n + 1}",
+    password: "sample#{n + 1}"
+  )
+  Book.create!(
+    user_id: "#{n + 1}",
+    title: "sample#{n + 1}",
+    body: "sample#{n + 1}"
+  )
+end
+
+
+
+# books = [
+#   [1, "リーダブルコード", "より良いコードを書くためのシンプルで実践的なテクニック"],
+#   [2, "トラブル知らずのシステム設計", "システム設計の要点を図解で説明してくれます"],
+#   [3, "たのしいRuby", "Rubyの入門におすすめです"],
+#   [4, "入門Git", "gitについての基本操作から内部構造まで解説してくれます"],
+#   [5, "アルゴリズム図鑑", "アルゴリズムについてカラーイラストでしっかり解説してくれます"],
+# ]
+
+# books.each do |user_id, title, body|
+#   Book.create!(
+#     user_id: user_id,
+#     title: title,
+#     body: body
+#   )
+# end
+
+
+
+
+

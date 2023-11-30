4bf5cfa6a3a5989e980176c610a884c4b4cd9b38
olkm-ren-sasaki <sasaki.ren.infratop@gmail.com>
2023-11-30 16:35:33 +0900

    [edit]raty load method change

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,7 +12,13 @@ import "popper.js"
 import "bootstrap"
 import '@fortawesome/fontawesome-free/js/all';
 import "../stylesheets/application" 
-require('packs/raty')
+// require('packs/raty')
+import Raty from "raty.js"
+window.raty = function(elem,opt) {
+  let raty =  new Raty(elem,opt)
+  raty.init();
+  return raty;
+}
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
